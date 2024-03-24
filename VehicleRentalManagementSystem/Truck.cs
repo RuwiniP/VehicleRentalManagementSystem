8fa5e5aed44f647b059421d212da4c0c2bea16df
ruwiniP <perera95ruwini@gmail.com>
2024-03-24 00:58:26 -0400

    Pushing the comepleted code for Truck class

--- VehicleRentalManagementSystem/Truck.cs
+++ VehicleRentalManagementSystem/Truck.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace VehicleRentalManagementSystem
 {
-    internal class Truck
+    public class Truck: Vehicle
     {
+
+        // Optional properties for Truck class.
+        // properties are all private and cannot be modified from outside the class
+        private int Capacity { get; set; }
+        private string TruckType { get; set; }
+        private bool FourWheelDrive { get; set; }
+
+        //Inherit vehicle class to get base class properties
+        public Truck(string model, string manufacturer, int year,
+            decimal rentalPrice, int capacity, string truckType,
+            bool fourWheelDrive) : base(model, manufacturer, year, rentalPrice)
+        {
+            Capacity = capacity;
+            TruckType = truckType;
+            FourWheelDrive = fourWheelDrive;
+        }
+
+        // Overriding Vehicle class (Vehicle) method to display base class properties
+        public override void DisplayDetails()
+        {
+            base.DisplayDetails();
+            Console.WriteLine($"Capacity: {Capacity} | Truck Type: {TruckType} | Four Wheel Drive: {(FourWheelDrive ? "Yes" : "No")}");
+        }
     }
 }
