78853b1e9e6304aefec5bcc4307145ef5be3e9f7
ruwiniP <perera95ruwini@gmail.com>
2024-03-24 00:33:43 -0400

    Add project files.

---
+++ VehicleRentalManagementSystem/Vehicle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleRentalManagementSystem
+{
+    internal class Vehicle
+    {
+        // Base class properties
+        private string Model { get; set; }
+        private string Manufacturer { get; set; }
+        private int Year { get; set; }
+
+        public readonly decimal RentalPrice; // This property can only be read by other classes
+
+        // Constructor of the base classs
+        protected Vehicle(string model, string manufacturer, int year, decimal rentalPrice)
+        {
+            Model = model;
+            Manufacturer = manufacturer;
+            Year = year;
+            RentalPrice = rentalPrice;
+        }
+
+        // Displaying base properties
+        public virtual void DisplayDetails()
+        {
+            Console.Write($"Model: {Model} | Manufacturer: {Manufacturer} | Year: {Year} | Rental Price: {RentalPrice} | ");
+        }
+    }
+}
